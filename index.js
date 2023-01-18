1c9cdaf03a4059219a778e767191f717b7ee8c82
Denis Pershakov <dpershakov@aimconsulting.com>
2023-01-18 11:40:53 -0800

    Mode azure storage account info out into a config file

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
 const azure = require('azure-storage');
+const config = require('config');
 
-const storageAccountName = 'dpershtestingsa01';
-const storageAccountKey = '+j1QHJ21qKEhi5I7hcBRah1lF/WxtggdG3U6prx2q76yi6XbPscm4Uj8a1zmGJNibnZbiQNXdkDI+AStnZNXKg==';
-//const containerName = 'dpershtesting-sa01-container01';
-//const blobName = 'CA.jpg';
-
+const storageAccountName = config.azure.storageAccountName;
+const storageAccountKey = config.azure.storageAccountKey;
 const containerNameParam = 'container';
 const blobNameParam = 'blob';
 
-
 function generateSignedUrl(containerName, blobName) {
 
-	const sharedAccessPolicy = {
-	  AccessPolicy: {
-		Permissions: azure.BlobUtilities.SharedAccessPermissions.READ,
-		Start: new Date(),
-		Expiry: azure.date.minutesFromNow(1),
-	  },
-	};
+    const sharedAccessPolicy = {
+        AccessPolicy: {
+            Permissions: azure.BlobUtilities.SharedAccessPermissions.READ,
+            Start: new Date(),
+            Expiry: azure.date.minutesFromNow(1),
+        },
+    };
 
-	const blobService = azure.createBlobService(storageAccountName, storageAccountKey);
-	const sasToken = blobService.generateSharedAccessSignature(containerName, blobName, sharedAccessPolicy);
-	const blobUrl = blobService.getUrl(containerName, blobName, sasToken);	
-	
-	return blobUrl;
+    const blobService = azure.createBlobService(storageAccountName, storageAccountKey);
+    const sasToken = blobService.generateSharedAccessSignature(containerName, blobName, sharedAccessPolicy);
+    const blobUrl = blobService.getUrl(containerName, blobName, sasToken);
+
+    return blobUrl;
 }
-
 
 var argv = require('minimist')(process.argv.slice(2));
 
 var container = argv[containerNameParam];
 var blob = argv[blobNameParam];
 
-
 console.log("Generating Shared Access Signature URL for " + "container: " + container + ", blob: " + blob);
-
 console.log(generateSignedUrl(container, blob));
